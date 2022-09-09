3dcace41ef43e43e22e5d3ea89c8b101b49fc235
AwdeDarkar <darkardengeno@gmail.com>
2022-09-09 14:51:53 -0400

    Add info footer to the bottom of the page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import TimerPage from "./TimerPage"
 
 import "./App.css"
 
+import packageJson from "../package.json"
+
 /**
  * The main app component.
  *
@@ -13,6 +15,28 @@ export default function App() {
     return (
         <div className="App">
             <TimerPage />
+            <Footer />
+        </div>
+    )
+}
+
+/**
+ * The footer of the app, which displays the version number and a link to the
+ * source code.
+ *
+ * @returns {Element} The footer of the app.
+ */
+function Footer() {
+    return (
+        <div className="Footer">
+            <p>
+                {`${packageJson.name} `}
+                version
+                {` ${packageJson.version}`}
+            </p>
+            <p>
+                <a href={packageJson.repository.url}>Source on Github</a>
+            </p>
         </div>
     )
 }
